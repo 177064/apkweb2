1556cb8975bd2d865d80cfad064768161e8b0d71
177064 <177064@stud.prz.edu.pl>
2025-10-28 10:04:40 +0000

    Com2_1.2. dodany loading

--- script.js
+++ script.js
@@ -15,6 +15,7 @@
   })
 
   cw1.addEventListener("click", function() {
+    answer.textContent = "Loading…"
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
