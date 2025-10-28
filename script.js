d469b2df36c939eac53f647e59788d208d7dc1c7
177064 <177064@stud.prz.edu.pl>
2025-10-28 10:02:08 +0000

    Com2_1.1.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
   const example = document.getElementById('example')
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
 
-  example.addEventListener("click", function () {
+  example.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
@@ -14,15 +14,27 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
+  cw1.addEventListener("click", function() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = array.map(post => `
+        <div>
+          <ul>
+          <li>${post.title}</li>
+          <li>${post.body}</li>
+        </ul>
+        </div>
+      `);
+      })
+  })
+
+  cw2.addEventListener("click", function() {
     //TODO
   })
 
-  cw2.addEventListener("click", function () {
-    //TODO
-  })
-
-  cw3.addEventListener("click", function () {
+  cw3.addEventListener("click", function() {
     //TODO
   })
 
